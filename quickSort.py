ec2d95897bcdbcabce7a86e061f496bcc787f05b
hesuhesu <zndwk0728@gmail.com>
2024-01-18 08:37:15 +0900

    add quickSort.py

---
+++ quickSort.py
@@ -0,0 +1,31 @@
+import sys
+
+def quick_sort(arr):
+    def sort(low, high):
+        if high <= low:
+            return
+
+        mid = partition(low, high)
+        sort(low, mid - 1)
+        sort(mid, high)
+
+    def partition(low, high):
+        pivot = arr[(low + high) // 2]
+        while low <= high:
+            while arr[low] < pivot:
+                low += 1
+            while arr[high] > pivot:
+                high -= 1
+            if low <= high:
+                arr[low], arr[high] = arr[high], arr[low]
+                low, high = low + 1, high - 1
+        return low
+
+    return sort(0, len(arr) - 1)
+
+print("list의 요소를 공백을 두고 입력하시오.")
+listA = list(map(int, sys.stdin.readline().split()))
+
+quick_sort(listA)
+
+print(listA)
